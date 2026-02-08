f6022c4cc78b6ebacf9324608b50bf7dca2700dc
Linhan202221156013 <3065693479@xiaomi.com>
2026-02-08 21:39:26 +0800

    实现编辑资料页面前端

--- frontend/src/stores/user.js
+++ frontend/src/stores/user.js
@@ -2,12 +2,12 @@ import {defineStore} from "pinia";
 import {ref} from "vue";
 
 export const useUserStore=defineStore('user',()=> {
-    const id=ref(1)
-    const username=ref('yxc')
-    const photo=ref('http://127.0.0.1:8000/media/user/photos/default.png')
-    const profile=ref('111')
-    const accessToken=ref('111')
-    const hasPulledUserInfo=ref('false')
+    const id=ref(0)
+    const username=ref('')
+    const photo=ref('')
+    const profile=ref('')
+    const accessToken=ref('')
+    const hasPulledUserInfo=ref(false)
     function isLogin(){
         return !!accessToken.value
     }
