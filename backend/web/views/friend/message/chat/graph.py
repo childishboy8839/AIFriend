176a1d18168d66fdae229f869a5147d2dad3c802
Linhan202221156013 <3065693479@xiaomi.com>
2026-03-11 10:32:53 +0800

    修复输入BUG

--- backend/web/views/friend/message/chat/graph.py
+++ backend/web/views/friend/message/chat/graph.py
@@ -20,30 +20,30 @@ from web.documents.utils.custom_embeddings import CustomEmbeddings
 
 class ChatGraph:
     @staticmethod
-    def create_app():
+    def create_app():#创建LangGraph应用
 
         @tool
-        def get_time() -> str:
+        def get_time() -> str:#工具函数，用于获取时间
             """当需要查询精确时间时，调用此函数。返回格式为：[年-月-日 时:分:秒]"""
             return localtime(now()).strftime('%Y-%m-%d %H:%M:%S')
 
         @tool
-        def search_knowledge_base(query: str) -> str:
+        def search_knowledge_base(query: str) -> str:#工具函数，用于查询知识库
             """当用户查询阿里云百炼平台的相关信息时，调用此函数。输入为要查询的问题，输出为查询结果。"""
             db = lancedb.connect('./web/documents/lancedb_storage')
             embeddings = CustomEmbeddings()
-            vector_db = LanceDB(
+            vector_db = LanceDB(#构建向量库
                 connection=db,
                 embedding=embeddings,
                 table_name='my_knowledge_base',
             )
-            docs = vector_db.similarity_search(query, k=3)
+            docs = vector_db.similarity_search(query, k=3)#相似度搜索，查找相似度最高的3项
             context = '\n\n'.join([f'内容片段：{i + 1}\n{doc.page_content}' for i, doc in enumerate(docs)])
             return f'从知识库中找到以下相关信息：\n\n{context}\n'
 
         tools = [get_time,search_knowledge_base]
 
-        llm = ChatOpenAI(
+        llm = ChatOpenAI(#llm初始化
             model='deepseek-v3.2',
             openai_api_key=os.getenv('API_KEY'),
             openai_api_base=os.getenv('API_BASE'),
@@ -53,24 +53,24 @@ class ChatGraph:
                     "include_usage": True,  # 输出token消耗数量
                 }
             }
-        ).bind_tools(tools)
+        ).bind_tools(tools)#绑定工具
 
         class AgentState(TypedDict):
             messages: Annotated[Sequence[BaseMessage], add_messages]
 
-        def model_call(state: AgentState) -> AgentState:
-            res = llm.invoke(state['messages'])
-            return {'messages': [res]}
+        def model_call(state: AgentState) -> AgentState:#AI思考节点
+            res = llm.invoke(state['messages'])#把聊天记录发给AI
+            return {'messages': [res]}#加入AI回复
 
-        def should_continue(state: AgentState) -> str:#路由节点
+        def should_continue(state: AgentState) -> str:#路由判断
             last_message = state['messages'][-1]
-            if last_message.tool_calls:
+            if last_message.tool_calls:#如果需要调用工具
                 return "tools"
             return "end"
 
-        tool_node=ToolNode(tools)
+        tool_node=ToolNode(tools)#工具节点，执行工具
         graph = StateGraph(AgentState)
-        graph.add_node('agent', model_call)
+        graph.add_node('agent', model_call)#创建节点
         graph.add_node('tools', tool_node)
 
         graph.add_edge(START, 'agent')
